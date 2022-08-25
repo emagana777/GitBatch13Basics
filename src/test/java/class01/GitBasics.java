9a8889adc3f735fb7a23afb32665b7307f7c4c84
emaga777 <emagana77@live.com>
2022-08-24 21:24:26 -0400

    added git ignore file

--- src/test/java/class01/GitBasics.java
+++ src/test/java/class01/GitBasics.java
@@ -2,6 +2,6 @@ package class01;
 
 public class GitBasics {
     public static void main(String[]args){
-        System.out.println("Today is a beautiful day");
+        System.out.println("Today is not a beautiful day");
     }
 }
